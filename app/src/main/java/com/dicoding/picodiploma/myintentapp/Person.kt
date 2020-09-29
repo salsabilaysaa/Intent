7b66b3126127f1bfef9bea315438ad5ad697f01e
salsabilaysaa <yarasalsabila28912@gmail.com>
2020-09-29 18:12:08 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/picodiploma/myintentapp/Person.kt
@@ -0,0 +1,20 @@
+package com.example.myintent
+
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Person (
+    val name: String?,
+    val age: Int?,
+    val email: String?,
+    val city: String?
+): Paracelable, Parcelable {
+
+}
+
+
+interface Paracelable {
+
+}
